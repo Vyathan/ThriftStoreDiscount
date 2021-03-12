8dee6f8989ad57a47151d46530661378a677f7b6
Matt Brundage II <mattjbrundage@icloud.com>
2021-03-12 11:54:18 -0500

    Code

--- ThriftStoreDiscount.cpp
+++ ThriftStoreDiscount.cpp
@@ -9,5 +9,20 @@ using namespace std;
 
 int main()
 {
-    
+    double itemPrice = 0.0;
+    double discountPrice = 0.0;
+    double discountAmt = 0.0;
+
+    cout << "Item Price: ";
+    cin >> itemPrice;
+
+    for (double rate = 0.1;
+    rate <= 0.4; rate += 0.05)
+    {
+        discountAmt = itemPrice * rate;
+        discountPrice = itemPrice - discountAmt;
+
+        cout << rate * 100 << "% discount: ";
+        cout << "$" << discountPrice << endl;
+    } //end for
 }
